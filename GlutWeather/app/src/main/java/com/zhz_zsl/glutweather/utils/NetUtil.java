3e06f8cd3b6c907f11179be0342604cc5fd34f3f
naku0426 <820774459@qq.com>
2019-05-22 09:53:33 +0800

    Update NetUtil.java

--- GlutWeather/app/src/main/java/com/zhz_zsl/glutweather/utils/NetUtil.java
+++ GlutWeather/app/src/main/java/com/zhz_zsl/glutweather/utils/NetUtil.java
@@ -5,25 +5,29 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 
+/**
+ * 网络状态工具类
+ */
 public class NetUtil {
-    public static final int NETWORN_NONE = 0;
-    public static final int NETWORN_WIFI = 1;
-    public static final int NETWORN_MOBILE = 2;
+    public static final int NETWORK_NONE = 0;   //无连接
+    public static final int NETWORK_WIFI = 1;   //使用WIFI
+    public static final int NETWORK_MOBILE = 2; //使用移动网络
 
     public static int getNetWorkState(Context context) {
         ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
-
+        //获取当前活动网络连接信息
         NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+
         if (networkInfo == null) {
-            return NETWORN_NONE;
+            return NETWORK_NONE;
         }
 
         int nType = networkInfo.getType();
         if (nType == ConnectivityManager.TYPE_MOBILE) {
-            return NETWORN_MOBILE;
+            return NETWORK_MOBILE;
         } else if (nType == ConnectivityManager.TYPE_WIFI) {
-            return NETWORN_WIFI;
+            return NETWORK_WIFI;
         }
-        return NETWORN_NONE;
+        return NETWORK_NONE;
     }
 }
